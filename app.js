cc5a877197f9968a0fcda34b6dc5792a7befa58b
Suryaraj Bhaduri <55868588+suryaraj1@users.noreply.github.com>
2020-11-11 10:33:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const totalCases = document.querySelector(".total_cases");
+const deaths = document.querySelector(".deaths");
+const recovered = document.querySelector(".recovered");
+const newCases = document.querySelector(".new_cases");
+
+const settings = {
+  async: true,
+  crossDomain: true,
+  url:
+    "https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+    "x-rapidapi-key": "856e1c77ffmshd41d1ea2db7d7d6p169deejsne12de99b5da2",
+  },
+};
+
+$.ajax(settings).done(function (response) {
+  displayData(response);
+});
+
+function displayData(data) {
+  const parsedData = JSON.parse(data);
+  console.log(parsedData.countries_stat);
+  const countries = parsedData.countries_stat;
+  countries.forEach(function (country) {
+    if (country.country_name == "India") {
+      totalCases.innerHTML = country.cases;
+      deaths.innerHTML = country.deaths;
+      recovered.innerHTML = country.total_recovered;
+      newCases.innerHTML = country.new_cases;
+    }
+  });
+}
